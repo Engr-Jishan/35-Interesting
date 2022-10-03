6a7694b2eb4a380a3d33da6bfc3551d37c9f1324
jisha66 <mydreamplace2020@gmail.com>
2022-10-03 13:12:35 +0600

    second page

--- type.js
+++ type.js
@@ -1,5 +1,13 @@
-let a = 29;
-console.log(typeof a);
+// let a = 29;
+// console.log(typeof a);
 
-let b = 'jisha ahmed';
-console.log(typeof b);
+// let b = 'jisha ahmed';
+// console.log(typeof b);
+
+const sum = 0;
+if (sum) {
+    console.log("variable is truthy");
+}
+else {
+    console.log("variable is falsy");
+}
