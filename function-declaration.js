354ba228bf987affeb43dbf041837f7027f90817
jisha66 <mydreamplace2020@gmail.com>
2022-10-08 21:34:07 +0600

    frst page

---
+++ function-declaration.js
@@ -0,0 +1,24 @@
+// function add(a, b) {
+//     console.log(a + b);
+// }
+// add(2, 3);
+
+// function declaration
+// function add(a, b) {
+//     console.log(a + b);
+// }
+// add(2, 2);
+
+
+// callback function
+// function greeting(name) {
+//     alert(`Hello, ${name}`);
+// }
+
+// function processUserInput(callback) {
+//     const name = prompt("Please enter your name.");
+//     callback(name);
+// }
+
+// processUserInput(greeting);
+
---
+++ undefined.js
@@ -0,0 +1,22 @@
+let first;
+// console.log(first);
+
+function second(x, y) {
+    const result = x + y;
+    return result;
+}
+const sum = second(2, 3);
+// return second();
+// console.log(result);
+// console.log(sum);
+
+function double(a, b, c) {
+    const result = a + b + c;
+    console.log(result);
+    return result;
+}
+// double(4, 4, 15);
+
+// Object
+const fifth = { name: "jishan ahemd", age: 20, location: "Narayanganj" };
+console.log(fifth.age);
