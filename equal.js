95f956c95c6759f09285630d2a5de8083a001316
jisha66 <mydreamplace2020@gmail.com>
2022-10-09 22:23:29 +0600

    closer page

--- equal.js
+++ equal.js
@@ -1,8 +1,16 @@
-const first = 2;
-const second = '2';
-if (first === second) {
-    console.log('conditon is ture');
+// const first = 2;
+// const second = '2';
+// if (first = == second) {
+//     console.log('conditon is ture');
+// }
+// else {
+//     console.log('conditon is false');
+// }
+
+function add(first, second) {
+    const result = first + second;
+    console.log(result);
+    return result;
 }
-else {
-    console.log('conditon is false');
-}
+const sum = add(11, 35);
+// console.log(result);
